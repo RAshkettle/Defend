e436c87dc2eb5a93a01c662058345b7c81ae481e
Richard Ashkettle <richard.ashkettle@gmail.com>
2025-06-26 19:26:27 -0400

    fixed player overwrite

--- player.go
+++ player.go
@@ -13,6 +13,11 @@ const (
 	LEFT
 )
 
+// Movement constants
+const (
+	PLAYER_MOVE_SPEED = 0.1
+)
+
 type Player struct {
 	X      float64
 	Y      float64
@@ -30,6 +35,14 @@ func NewPlayer() *Player {
 }
 
 func (p *Player) Update(camera *Camera, terrainWidth float64) error {
+	screenWidth := int(camera.Width)
+	minimapWidth := screenWidth / 3
+	minimapX := (screenWidth - minimapWidth) / 2
+	leftEdge := float64(minimapX)
+	rightEdge := float64(minimapX + minimapWidth)
+	minY := 35.0
+	maxY := float64(camera.Height - 10)
+
 	if ebiten.IsKeyPressed(ebiten.KeyLeft) || ebiten.IsKeyPressed(ebiten.KeyA) {
 		camera.MoveLeft(terrainWidth)
 		p.Facing = LEFT
@@ -39,6 +52,28 @@ func (p *Player) Update(camera *Camera, terrainWidth float64) error {
 		camera.MoveRight(terrainWidth)
 		p.Facing = RIGHT
 	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowUp) || ebiten.IsKeyPressed(ebiten.KeyW) {
+		p.Y -= 2
+		if p.Y < minY {
+			p.Y = minY
+		}
+	}
+
+	if ebiten.IsKeyPressed(ebiten.KeyArrowDown) || ebiten.IsKeyPressed(ebiten.KeyS) {
+		p.Y += 2
+		if p.Y > maxY {
+			p.Y = maxY
+		}
+	}
+
+	targetX := leftEdge
+	if p.Facing == LEFT {
+		targetX = rightEdge
+	}
+
+	distToTarget := targetX - p.X
+	p.X += distToTarget * PLAYER_MOVE_SPEED
 
 	return nil
 }
