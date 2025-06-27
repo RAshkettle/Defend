277a2a9314ef29b33925e92d38551702889505bc
Richard Ashkettle <richard.ashkettle@gmail.com>
2025-06-27 10:38:20 -0400

    shooting sounds

--- assets/assets.go
+++ assets/assets.go
@@ -6,12 +6,20 @@ import (
 	"image"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/audio"
+	"github.com/hajimehoshi/ebiten/v2/audio/wav"
 )
 
 //go:embed *
 var assets embed.FS
 
 var PlayerSprite = loadImage("sprites/player.png")
+var AudioContext = audio.NewContext(44100)
+
+var LaserSound = loadPlayerFromWav("audio/laser.wav", 0.3)
+
+
+
 
 func loadImage(filePath string) *ebiten.Image {
 	data, err := assets.ReadFile(filePath)
@@ -26,4 +34,24 @@ func loadImage(filePath string) *ebiten.Image {
 
 	ebitenImg := ebiten.NewImageFromImage(img)
 	return ebitenImg
+}
+
+func loadPlayerFromWav(filePath string, volume float64) *audio.Player {
+	data, err := assets.ReadFile(filePath)
+	if err != nil {
+		panic(err)
+	}
+
+	stream, err := wav.DecodeWithSampleRate(AudioContext.SampleRate(), bytes.NewReader(data))
+	if err != nil {
+		panic(err)
+	}
+
+	player, err := AudioContext.NewPlayer(stream)
+	if err != nil {
+		panic(err)
+	}
+	player.SetVolume(volume)
+
+	return player
 }
